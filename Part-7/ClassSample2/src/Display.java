3825e575ebfe8d199e2593cefeeb93e428d42195
Akhil  Binu <akhilbinuwakeup@gmail.com>
2023-07-21 09:59:51 +0530

    update

---
+++ Part-7/ClassSample2/src/Display.java
@@ -0,0 +1,21 @@
+
+public class Display {
+	public static void main(String ar[]) {
+		
+		Sample s1=new Sample();
+		Sample s2=new Sample();
+		
+		s1.a=10;
+		s1.b=20;
+		s2.a=30;
+		s2.b=40;
+		
+		
+		s1.display();
+		s2.display();
+		s1.sum();
+		s2.sum(); 
+		
+	}
+
+}
---
+++ Part-7/ClassSample2/src/Sample.java
@@ -0,0 +1,13 @@
+
+public class Sample {
+	int a,b;
+	int c;
+	void sum(){
+		c=a+b;
+	}
+	void display() {
+	System.out.println("a : "+a+" b : "+b);
+	System.out.println("sum is : "+c);
+	}
+
+}
